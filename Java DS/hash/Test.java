636bcd7f3b7ee7b34572220bec57b529a5d47587
calljsh <144641409+calljsh@users.noreply.github.com>
2024-12-23 21:09:00 +0800

    Add files via upload

---
+++ Java DS/hash/Test.java
@@ -0,0 +1,29 @@
+package hash;
+
+
+import java.util.Objects;
+
+class  person
+{
+    public int age;
+    public String name;
+
+    public person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(name);
+//    }
+}
+
+
+public class Test
+{
+    public static void main(String[] args) {
+
+    }
+}
+
